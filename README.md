0479f8a8ddad66ffefb9630ed27d5641b039e16c
Pankaj Kumar <pankaj.kmr1990@gmail.com>
2020-06-26 11:40:22 +0530

    modifieded gan

--- README.md
+++ README.md
@@ -4,5 +4,5 @@ Some deep learning algorithms from scratch.
 Implemented algorithms include:
 
 - Self Organising Map (SOM)
-- Variational AutoEncoder (VAE)
-- Generative Adversarial Network (GAN)
+- [Variational AutoEncoder (VAE)](https://keras.io/examples/generative/vae/)
+- [Generative Adversarial Network (GAN)](https://towardsdatascience.com/gan-by-example-using-keras-on-tensorflow-backend-1a6d515a60d0)
--- gan.py
+++ gan.py
@@ -12,7 +12,6 @@ from tensorflow.keras.optimizers import Adam, RMSprop
 
 class GAN(object):
     def __init__(self, img_rows=28, img_cols=28, channel=1):
-
         self.img_rows = img_rows
         self.img_cols = img_cols
         self.channel = channel
@@ -21,31 +20,24 @@ class GAN(object):
         self.AM = None  # adversarial model
         self.DM = None  # discriminator model
 
-    # (W−F+2P)/S+1
     def discriminator(self):
         if self.D:
             return self.D
-        self.D = Sequential()
+        self.D = Sequential(name='Discriminator')
         depth = 64
         dropout = 0.4
         # In: 28 x 28 x 1, depth = 1
         # Out: 14 x 14 x 1, depth=64
         input_shape = (self.img_rows, self.img_cols, self.channel)
+
         self.D.add(Conv2D(depth*1, 5, strides=2, input_shape=input_shape, padding='same'))
         self.D.add(LeakyReLU(alpha=0.2))
         self.D.add(Dropout(dropout))
 
-        self.D.add(Conv2D(depth*2, 5, strides=2, padding='same'))
-        self.D.add(LeakyReLU(alpha=0.2))
-        self.D.add(Dropout(dropout))
-
-        self.D.add(Conv2D(depth*4, 5, strides=2, padding='same'))
-        self.D.add(LeakyReLU(alpha=0.2))
-        self.D.add(Dropout(dropout))
-
-        self.D.add(Conv2D(depth*8, 5, strides=1, padding='same'))
-        self.D.add(LeakyReLU(alpha=0.2))
-        self.D.add(Dropout(dropout))
+        for i in [2, 4, 8]:
+            self.D.add(Conv2D(depth * i, 5, strides=2, padding='same'))
+            self.D.add(LeakyReLU(alpha=0.2))
+            self.D.add(Dropout(dropout))
 
         # Out: 1-dim probability
         self.D.add(Flatten())
@@ -57,9 +49,9 @@ class GAN(object):
     def generator(self):
         if self.G:
             return self.G
-        self.G = Sequential()
+        self.G = Sequential(name='Generator')
         dropout = 0.4
-        depth = 64+64+64+64
+        depth = 256
         dim = 7
         # In: 100
         # Out: dim x dim x depth
@@ -71,19 +63,12 @@ class GAN(object):
 
         # In: dim x dim x depth
         # Out: 2*dim x 2*dim x depth/2
-        self.G.add(UpSampling2D())
-        self.G.add(Conv2DTranspose(int(depth/2), 5, padding='same'))
-        self.G.add(BatchNormalization(momentum=0.9))
-        self.G.add(Activation('relu'))
-
-        self.G.add(UpSampling2D())
-        self.G.add(Conv2DTranspose(int(depth/4), 5, padding='same'))
-        self.G.add(BatchNormalization(momentum=0.9))
-        self.G.add(Activation('relu'))
-
-        self.G.add(Conv2DTranspose(int(depth/8), 5, padding='same'))
-        self.G.add(BatchNormalization(momentum=0.9))
-        self.G.add(Activation('relu'))
+        for i in [2, 4, 8]:
+            if i in [2, 4]:
+                self.G.add(UpSampling2D())
+            self.G.add(Conv2DTranspose(int(depth / i), 5, padding='same'))
+            self.G.add(BatchNormalization(momentum=0.9))
+            self.G.add(Activation('relu'))
 
         # Out: 28 x 28 x 1 grayscale image [0.0,1.0] per pix
         self.G.add(Conv2DTranspose(1, 5, padding='same'))
@@ -94,18 +79,18 @@ class GAN(object):
     def discriminator_model(self):
         if self.DM:
             return self.DM
-        optimizer = RMSprop(lr=0.0002, decay=6e-8)
         self.DM = Sequential()
         self.DM.add(self.discriminator())
+        optimizer = RMSprop(lr=0.0002, decay=6e-8)
         self.DM.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=['accuracy'])
         return self.DM
 
     def adversarial_model(self):
         if self.AM:
             return self.AM
-        optimizer = RMSprop(lr=0.0001, decay=3e-8)
         self.AM = Sequential()
         self.AM.add(self.generator())
         self.AM.add(self.discriminator())
+        optimizer = RMSprop(lr=0.0001, decay=3e-8)
         self.AM.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=['accuracy'])
         return self.AM
